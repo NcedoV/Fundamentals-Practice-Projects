f430037f4ffac8583b21a6c05a03bde54def2495
Ncedo Vilakazi <vncedo@gmail.com>
2025-03-26 17:10:00 +0200

    Forgot to add actual programme files. Included here.

--- src/temperatureconverter/TemperatureConverterApp.java
+++ src/temperatureconverter/TemperatureConverterApp.java
@@ -1,4 +1,8 @@
 package temperatureconverter;
 
 public class TemperatureConverterApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Temperature Unit Converter!");
+
+    }
 }
--- test/temperatureconverter/TemperatureConverterAppTest.java
+++ test/temperatureconverter/TemperatureConverterAppTest.java
@@ -1,4 +1,12 @@
 package temperatureconverter;
 
+import org.junit.jupiter.api.Test; //This import requires JUnit 5
+
+import static org.junit.jupiter.api.Assertions.assertTrue; //This import requires JUnit 5
+
 public class TemperatureConverterAppTest {
+    @Test
+    void placeholderTest() {
+        assertTrue(true); // A basic test that always passes. Replace this later!
+    }
 }
